eb52f4142e1c414d165225836d3960cc93b9dc02
Lucas Abreu <contato@agenciaavance.com>
2019-06-15 03:42:06 -0300

    teste

--- menudino/dm-footer.js
+++ menudino/dm-footer.js
@@ -10,7 +10,7 @@ function addBadge() {
     var div = '.categories';
     var word = '<div class="name">Promo |';
     div = typeof (div) == 'string' ? document.querySelector(div) : div;
-    div.innerHTML = div.innerHTML.replace(word, '<div class="name"><div class="teste">Promo</div>');
+    div.innerHTML = div.innerHTML.replace(word, '<div class="teste"><div class="name">Promo</div>');
 }
 addBadge();
 
