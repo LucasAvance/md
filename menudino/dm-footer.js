a46abdccdb8ee4999f9232f6b1b87181b4d6d83c
Lucas Abreu <contato@agenciaavance.com>
2019-06-28 23:19:08 -0300

    Logo Color Tiny

--- menudino/dm-footer.js
+++ menudino/dm-footer.js
@@ -72,6 +72,10 @@ $(window).on('load', function() {
     $('#promoDeuMatch').modal('show');
 });*/
 
+
+/* Inserção de Conteúdo - Logo Reduzido */
+$(".navbar-brand").prepend('<div class="brand-logo-t"><img src="/menudino/images/logo-color-t.png" alt="Deu Match Burguer" class="img-responsive"></div>');
+
 /* Inserção de Conteúdo - Body */
 $("body").append('<div class="status_restaurante"></div>');
 
