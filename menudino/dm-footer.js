9c696651ab18addba78d2021195a9aea753d66fe
Lucas Abreu <contato@agenciaavance.com>
2019-10-04 17:43:21 -0300

    Endereço

--- menudino/dm-footer.js
+++ menudino/dm-footer.js
@@ -98,8 +98,6 @@ $(".cardapio-body").prepend('<ul class="dm-infos"><li><h3>Endereço para retirad
 $("#detalhesAtendimentoModal .bg-danger").prepend('<i class="fa fa-clock-o"></i>');
 
 
-/* Alteração de Localização - Endereço */
-$("address").clone().prependTo(".endereco");
 
 /* Alteração de Localização - Tempo Entrega */
 $(".tempoEstimadoSelected").clone().prependTo(".tempoentrega");
